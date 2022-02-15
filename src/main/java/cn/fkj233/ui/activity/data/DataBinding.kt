519f9703e16a72c833f2206e5632869336e1e08c
方块君 <fkj@fkj233.cn>
2022-02-15 20:05:30 +0800

    LGPL

--- src/main/java/cn/fkj233/ui/activity/data/DataBinding.kt
+++ src/main/java/cn/fkj233/ui/activity/data/DataBinding.kt
@@ -4,7 +4,7 @@
  * https://github.com/577fkj/BlockMIUI
  *
  * This software is free opensource software: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
+ * and/or modify it under the terms of the GNU Lesser General Public License v2.1
  * as published by the Free Software Foundation; either
  * version 3 of the License, or any later version and our eula as published
  * by 577fkj.
@@ -12,9 +12,9 @@
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * GNU General Public License for more details.
+ * GNU Lesser General Public License v2.1 for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License v2.1
  * and eula along with this software.  If not, see
  * <https://www.gnu.org/licenses/>
  * <https://github.com/577fkj/BlockMIUI/blob/main/LICENSE>.
